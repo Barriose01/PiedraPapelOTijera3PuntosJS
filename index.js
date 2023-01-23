952c56b76ce061e6142e0ec472d9b340afa7b402
Eddie Casañas <107152796+Barriose01@users.noreply.github.com>
2023-01-23 09:48:52 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 let txtRonda = document.getElementById("ronda");
 let ronda = 0;
-let pc = document.getElementById("pc");
-let usuario = document.getElementById("usuario");
 let ganador = document.getElementById("ganador");
-let botones = document.querySelectorAll("input");
 let txtPuntosUsuario = document.getElementById("puntosUsuario");
 let txtPuntosPC = document.getElementById("puntosPC");
 let puntosUsuario = 0;
 let puntosPC = 0;
-let botonJugar = document.getElementById("btnJugada");
-let botonEmpezar = document.getElementById("btnComenzar");
+let bloquePC = document.getElementById("bloquePC");
+let bloqueUsuario = document.getElementById("bloqueUsuario");
+let bloques = document.querySelectorAll("article");
+let botones = document.querySelectorAll(".botones-accion");
+let btnVolverJugar = document.getElementById("btnVolverJugar");
 
-botonJugar.addEventListener("click",jugar);
-botonEmpezar.addEventListener("click",volverAJugar);
+botones.forEach((boton) =>{
+    boton.addEventListener("click",jugar);
+});
+
+btnVolverJugar.addEventListener("click",volverAJugar);
 
 function generarOpcionPC(){
     let opcionPC = Math.floor(Math.random() * 3) + 1;
@@ -31,49 +34,66 @@ function generarOpcionPC(){
     return opcionString;
 }
 
-function generarOpcionUsuario(){
-    let opcionUsuario = "";
-    botones.forEach((boton) =>{
-        if(boton.checked){
-            opcionUsuario = boton.value;
-        }
-    })
-    return opcionUsuario;
-}
-
 function darPuntuacion(opcionUsuario,opcionPC){
+    let imagenUsuario = document.createElement("img");
+    let imagenPC = document.createElement("img");
     switch(opcionUsuario){
         case "Piedra":
+            imagenUsuario.src = "piedra.jpg";
             if(opcionPC == "Papel"){
+                imagenPC.src = "papel.jpg";
                 puntosPC +=1;
             }else if(opcionPC == "Tijera"){
+                imagenPC.src = "tijera.jpg";
                 puntosUsuario += 1;
+            }else{
+                imagenPC.src = "piedra.jpg";
             }
             break;
         case "Papel":
+            imagenUsuario.src = "papel.jpg";
             if(opcionPC == "Tijera"){
+                imagenPC.src = "tijera.jpg";
                 puntosPC +=1;
             }else if(opcionPC == "Piedra"){
+                imagenPC.src = "piedra.jpg";
                 puntosUsuario +=1;
+            }else{
+                imagenPC.src = "papel.jpg";
             }
             break;
         case "Tijera":
+            imagenUsuario.src = "tijera.jpg";
             if(opcionPC == "Piedra"){
+                imagenPC.src = "piedra.jpg";
                 puntosPC += 1;
             }else if(opcionPC == "Papel"){
+                imagenPC.src = "papel.jpg";
                 puntosUsuario += 1;
+            }else{
+                imagenPC.src = "tijera.jpg";
             }
             break;
     }
+    verificarBloquesImagenes();
+    bloquePC.append(imagenPC);
+    bloqueUsuario.append(imagenUsuario);
     txtPuntosPC.textContent = "Puntos Maquina: " + puntosPC;
     txtPuntosUsuario.textContent = "Puntos Usuario: " + puntosUsuario;
     verificarGanador();
 }
 
+function verificarBloquesImagenes(){
+    bloques.forEach((bloque) => {
+        if(bloque.childElementCount > 1){
+            bloque.removeChild(bloque.lastChild);
+        }
+    })
+}
+
 function verificarGanador(){
     if(puntosPC == 3 || puntosUsuario == 3){
-        botonJugar.disabled = true;
-
+        botones.forEach((boton) => {boton.disabled = true});
         if(puntosPC == 3){
             ganador.textContent = "La maquina ha ganado";
         }else if(puntosUsuario == 3){
@@ -83,28 +103,20 @@ function verificarGanador(){
 }
 
 function volverAJugar(){
-    botonJugar.disabled = false;
+    botones.forEach((boton) => {boton.disabled = false});
     ganador.textContent = "";
     ronda = puntosPC = puntosUsuario = 0;
     txtRonda.textContent = "Ronda 0";
     txtPuntosPC.textContent = "Puntos Maquina: 0";
     txtPuntosUsuario.textContent = "Puntos Usuario: 0";
-    pc.textContent = "Maquina:";
-    usuario.textContent = "Usuario:";
-    botones.forEach((boton) => boton.checked = false);
+    verificarBloquesImagenes();
 }
 
 function jugar(){
-    let opcionUsuario = generarOpcionUsuario();
+    let opcionUsuario = this.textContent; //Obtiene el texto del boton presionado
     let opcionPC = generarOpcionPC();
-    if(opcionUsuario == ""){
-        alert("Debe seleccionar una opcion para poder jugar");
-    }else{
-        ronda += 1;
-        txtRonda.textContent = "Ronda " +  ronda;
-        pc.textContent = "Maquina: " + opcionPC;
-        usuario.textContent = "Usuario: " + opcionUsuario;
-        darPuntuacion(opcionUsuario,opcionPC);
-    }
+    ronda += 1;
+    txtRonda.textContent = "Ronda " +  ronda;
+    darPuntuacion(opcionUsuario,opcionPC);
 }
 
